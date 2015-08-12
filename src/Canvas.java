d0a703ee9c9de492ea063e745399a5ca51c38c2a
stefan4472 <stefan.kussmaul@gmail.com>
2015-08-12 19:43:15 -0400

    Basic Swing GUI

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Stefan on 8/12/2015.
  */
 public class Canvas extends JPanel {
+
+    private BufferedImage display;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Stefan on 8/12/2015.
  */
 public class Main {
+
+    private JFrame frame;
+
+    private Canvas canvas;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setUpGUI();
+    }
+
+    private void setUpGUI() {
+        frame = new JFrame("AnimationTest");
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(500, 500));
+        frame.getContentPane().add(canvas, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
