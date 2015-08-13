644ff4c98d2d0a8617d9ec74a95ef072ce2b2125
stefan4472 <stefan.kussmaul@gmail.com>
2015-08-13 15:27:45 -0400

    More Changes

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 /**
  * Created by Stefan on 8/13/2015.
  */
@@ -6,6 +8,8 @@ public class Rocket extends Sprite {
     private final int BOARD_WIDTH = 390;
     private final int MISSILE_SPEED = 2;
     private float acceleration;
+
+    private SpriteAnimation startMoving;
 
     public Rocket(int x, int y) {
         super(x, y);
@@ -17,13 +21,18 @@ public class Rocket extends Sprite {
         loadDefaultImage("rocket.png");
         getImageDimensions();
         acceleration = 0;
+
+        startMoving = new SpriteAnimation(new File[] {
+                new File("rocket.png"),
+                new File("rocket_starting1.png"),
+        }, false);
     }
 
 
     public void move() {
         x += MISSILE_SPEED + acceleration;
         acceleration += 0.2;
-        System.out.println("Acceleration is " + acceleration);
+
         if (x > BOARD_WIDTH) {
             vis = false;
         }
