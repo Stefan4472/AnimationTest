3e806c6488341c5ce2b52ca14295e083b7b7e308
stefan4472 <stefan.kussmaul@gmail.com>
2015-08-17 11:49:45 -0400

    Better Sprites and Refactored Acceleration

--- src/Spaceship.java
+++ src/Spaceship.java
@@ -64,26 +64,17 @@ public class Spaceship extends Sprite {
 
     //
     public void move() {
-        System.out.println("dx = " + dx);
-        // sprite direction not equal to zero
-        if(dx != 0 || dy != 0) {
-            // sprite was previously not moving. Play startmoving animation and reset acceleration
+        // either accelerating or breaking
+        if(dx != 0) {
+            // sprite was previously not accelerating/breaking. Play startMovingAnimation
             if(moving == false && dx == 1) {
                 currentImage = startMovingAnimation.start();
-            } else { // sprite was previously moving. Figure out speed
-                if(startMovingAnimation.isPlaying()) {
+            } else { // sprite was previously moving play startMovingAnimation if it's playing or
+                // spaceship is breaking but has not stopped completely
+                if(startMovingAnimation.isPlaying() || dx == -1 && speedX != 0.0) {
                     currentImage = startMovingAnimation.nextFrame();
-                    if(dx == 1 && speedX < MAX_SPEED_X)
-                        speedX += 0.03;
-                    if(dy == 1 && speedY < MAX_SPEED_Y)
-                        speedY += 0.075;
-                    else if(dy == -1 && speedY > -MAX_SPEED_Y)
-                        speedY -= 0.075;
-                } else { // Play moving animation as soon as startmoving animation is over
-                    currentImage = movingAnimation.nextFrame(); // todo: engine animation is off
-
-                    speedX = calcSpeedX(dx, speedX); // todo: accelerate, brake, drift functions and much simpler if/else branch
-                    //speedY = calcSpeedY();
+                } else if(dx == 1){ // Play moving animation as soon as startmoving animation is over
+                    currentImage = movingAnimation.nextFrame(); // todo: animations
                 }
             }
             moving = true;
@@ -92,17 +83,17 @@ public class Spaceship extends Sprite {
             moving = false;
         }
 
-        /*if(moving == true && acceleration < 0.1)
-            acceleration += 0.007;
-        else if(moving == true && acceleration < 2.0)
-            acceleration += 0.05;
-        else if(moving == true && acceleration < 3.0)
-            acceleration += 0.08;
-        else if(moving == false && acceleration >= 0.0)
-            acceleration -= 0.05; */
-        System.out.println("Speed is " + speedX + " px per frame!!!");
+        if(dx == 1)
+            accelerate();
+        else if(dx == 0)
+            drift();
+        else if(dx == -1)
+            applyBreak();
+
         x += speedX;
-        y += speedY;
+
+        if(speedX != 0) // can only move vertically when speed != 0
+            y += dy;
     }
 
     // fires rocket
@@ -113,51 +104,53 @@ public class Spaceship extends Sprite {
         rockets.add(r2);
     }
 
-    private float calcSpeedX(int dx, float speedX) {
-        if(dx == 1 && speedX <= MAX_SPEED_X) {
-            if(speedX <= 1.0) {
-                speedX += 0.04;
-            } else if(speedX <= 3.0) {
-              speedX += 0.07;
-            } else if(speedX <= 4.0) {
-                speedX += 0.025;
-            } else if(speedX <= 7.5){
-                speedX += 0.0125;
-            } else {
-                speedX += .001;
-            }
-        } else if(dx == -1 && speedX >= 0) {
-            if(speedX >= 3.5) {
-                speedX -= 0.05;
-            } else if(speedX >= 2.0) {
-                speedX -= 0.065;
-            } else if(speedX >= 1.0) {
-                speedX -= .025;
-            } else {
-                speedX = 0;
-            }
-        } else if(dx == 0 && speedX >= 0) {
-            System.out.println("Slowing down");
-            if(speedX > 8.9) {
-                speedX -= 0.0002;
-            } else if(speedX > 8.5) {
-                speedX -= 0.0005;
-            } else if(speedX > 7.5) {
-                speedX -= 0.001;
-            } else if(speedX > 5.0) {
-                speedX -= 0.005;
-            } else if(speedX > 2.0) {
-                speedX -= 0.01;
-            } else if(speedX > 1.0){
-                speedX -= 0.03;
-            }
-
-        }
-
-        if(speedX > MAX_SPEED_X)
+    // manages speed when dx = 1
+    private void accelerate() {
+        if(speedX <= 1.0) {
+            speedX += 0.05;
+        } else if(speedX <= 3.0) {
+            speedX += 0.1;
+        } else if(speedX <= 4.0) {
+            speedX += 0.05;
+        } else if(speedX <= 7.5){
+            speedX += 0.0125;
+        } else if(speedX <= MAX_SPEED_X){
+            speedX += .001;
+        } else if(speedX > MAX_SPEED_X) {
             speedX = MAX_SPEED_X;
-        return speedX;
+        }
     }
+
+    // manages speed when dx = -1
+    private void applyBreak() {
+        if(speedX >= 3.5) {
+            speedX -= 0.05;
+        } else if(speedX >= 2.0) {
+            speedX -= 0.065;
+        } else if(speedX >= 1.0) {
+            speedX -= .025;
+        } else {
+            speedX = 0;
+        }
+    }
+
+    // manages speed when dx = 0
+    private void drift() {
+        if (speedX > 8.5) {
+            speedX -= 0.005;
+        } else if (speedX > 7.5) {
+            speedX -= 0.008;
+        } else if (speedX > 5.0) {
+            speedX -= 0.005;
+        } else if (speedX > 2.0) {
+            speedX -= 0.01;
+        } else if (speedX >= 1.0) {
+            speedX -= 0.025;
+        } else {
+            speedX = 0;
+        }
+    }
+
 
     //private float calcSpeedY(int dy, float speedY) {
 
@@ -202,11 +195,11 @@ public class Spaceship extends Sprite {
         }
 
         if (key == KeyEvent.VK_UP) {
-            dy = -1;
+            dy = -2;
         }
 
         if (key == KeyEvent.VK_DOWN) {
-            dy = 1;
+            dy = 2;
         }
 
         if(key == KeyEvent.VK_SPACE)
