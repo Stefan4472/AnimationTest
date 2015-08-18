17ce7192eef096f550b686b0723e899b110cfb56
stefan4472 <stefan.kussmaul@gmail.com>
2015-08-18 12:00:30 -0400

    Added Untracked Files

---
+++ src/Bullet.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+/**
+ * Created by Stefan on 8/17/2015.
+ */
+public class Bullet extends Sprite {
+    private final int BOARD_WIDTH = 600;
+    private final int MISSILE_SPEED = 6;
+
+    public Bullet(int x, int y) {
+        super(x, y);
+
+        initBullet();
+    }
+
+    private void initBullet() {
+        loadDefaultImage("rocket.png");
+        getImageDimensions();
+    }
+
+
+    public void move() {
+        x += MISSILE_SPEED;
+
+        if (x > BOARD_WIDTH) {
+            vis = false;
+        }
+    }
+}
