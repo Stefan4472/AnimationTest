0ed2ca0443c7df4ef2ccc7bfcb2134271bce9b70
stefan4472 <stefan.kussmaul@gmail.com>
2015-08-28 21:41:32 -0400

    Initial Commit

---
+++ src/Coin.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Stefan on 8/28/2015.
+ */
+public class Coin extends Sprite {
+
+    public Coin(String imageName) {
+        super(imageName);
+        initObstacle();
+    }
+
+    public Coin(String imageName, int x, int y) {
+        super(imageName, x, y);
+        initObstacle();
+    }
+
+    private void initObstacle() {
+        hitBox.setOffsets(5, 5);
+        hitBox.setDimensions(40, 40);
+    }
+
+    public void updateCurrentImage() {
+
+    }
+
+    public void updateActions() {
+
+    }
+
+    public void updateSpeeds() {
+
+    }
+
+    public void handleCollision(Sprite s) {
+        vis = false;
+    }
+}
