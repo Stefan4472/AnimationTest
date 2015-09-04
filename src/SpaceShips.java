e4078eb141a7609871f86d8fd4397d16660cea67
stefan4472 <stefan.kussmaul@gmail.com>
2015-09-04 16:02:05 -0400

    ImageUtil getSpriteSheetDiff

--- src/SpaceShips.java
+++ src/SpaceShips.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Stefan on 8/12/2015.
@@ -10,13 +11,14 @@ public class Spaceships extends JFrame {
     private final int SCREEN_HEIGHT = 300;
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
+        ImageUtil.getSpriteSheetDiff("spaceship.png", "spaceship_firing_spritesheet.png", "spaceship_firing_spritesheet_diff.png");
+        /*EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
                 Spaceships ex = new Spaceships();
                 ex.setVisible(true);
             }
-        });
+        });*/
     }
 
     public Spaceships() {
