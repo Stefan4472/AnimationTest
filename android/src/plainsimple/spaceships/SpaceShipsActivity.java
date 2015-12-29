79d93f245d4a6473c4b55d1c7c354d417b43a84c
stefan4472 <stefan.kussmaul@gmail.com>
2015-12-28 22:29:20 -0500

    More Experimenting With Layouts

--- android/src/plainsimple/spaceships/SpaceShipsActivity.java
+++ android/src/plainsimple/spaceships/SpaceShipsActivity.java
@@ -2,6 +2,8 @@ package plainsimple.spaceships;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,14 +14,13 @@ public class SpaceShipsActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TitleView title_view = new TitleView(this);
-        //title_view.setKeepScreenOn(true);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //setContentView(title_view);
         setContentView(R.layout.mainscreen_layout);
-        //titleView = (TitleView) findViewById(R.id.spaceships);
-        //titleView.setKeepScreenOn(true);
+    }
+
+    private void onPlayPressed(View view) {
+        Log.d("Main Class", "Hi");
     }
 }
--- android/src/plainsimple/spaceships/TitleView.java
+++ android/src/plainsimple/spaceships/TitleView.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Stefan on 10/17/2015.
@@ -22,7 +23,10 @@ public class TitleView extends View {
     private int screenW;
     private int screenH;
     private Context context;
-    private Button playNowBtn = (Button) findViewById(R.id.playbutton);
+
+    private ImageButton playButton = (ImageButton) findViewById(R.id.playbutton);
+    private Button storeButton = (Button) findViewById(R.id.storebutton);
+    private Button statsButton = (Button) findViewById(R.id.statsbutton);
 
 
     public TitleView(Context context) {
@@ -57,6 +61,7 @@ public class TitleView extends View {
         }
     }
 
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
         //int event_action = event.getAction();
         int x = (int) event.getX();
@@ -81,6 +86,21 @@ public class TitleView extends View {
         }
         invalidate();
         return true;
+    }
+
+    // handles play button pressed
+    private void onPlayPressed(View view) {
+        Log.d("TitleView Class", "Play");
+    }
+
+    // handles store button pressed
+    private void onStorePressed(View view) {
+
+    }
+
+    // handles stats button pressed
+    private void onStatsPressed(View view) {
+
     }
 
     // starts the game activity
