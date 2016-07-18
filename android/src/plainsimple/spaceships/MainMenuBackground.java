0c0b841da853f9fa7978297f592241f679ff5ab9
stefan4472 <stefan.kussmaul@gmail.com>
2016-07-18 07:17:47 -0400

    Added Button SVGs by Chao Zheng Li, Not Implemented

--- android/src/plainsimple/spaceships/MainMenuBackground.java
+++ android/src/plainsimple/spaceships/MainMenuBackground.java
@@ -6,7 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
-import plainsimple.imagetransition.FadeInTransition;
+import plainsimple.imagetransition.SlideInTransition;
 
 /**
  * Created by Stefan on 6/28/2016.
@@ -14,26 +14,24 @@ import plainsimple.imagetransition.FadeInTransition;
 public class MainMenuBackground extends View {
 
     private Bitmap backgroundImg;
-    private FadeInTransition transition;
+    private SlideInTransition transition;
     private int width;
     private int height;
 
     public MainMenuBackground(Context context, AttributeSet attrs) {
         super(context, attrs);
+        backgroundImg = BitmapFactory.decodeResource(this.getResources(), R.drawable.space_background);
     }
 
     @Override
     public void onDraw(Canvas canvas) { // todo: may require invalidate() to force animation update
-        canvas.drawBitmap(transition.nextFrame(), 0, 0, null);
-        invalidate();
+        canvas.drawBitmap(backgroundImg, 0, 0, null);
     }
 
     @Override
     public void onSizeChanged(int w, int h, int oldw, int oldh) {
         width = w;
         height = h;
-        backgroundImg = BitmapFactory.decodeResource(this.getResources(), R.drawable.space_background);
         backgroundImg = Bitmap.createScaledBitmap(backgroundImg, width, height, true);
-        transition = new FadeInTransition(null, backgroundImg, 100);
     }
 }
