c2afd1159b42c4b205e039adf51232b6ccc9721e
stefan4472 <stefan.kussmaul@gmail.com>
2017-05-16 23:15:41 -0400

    Small GameMode/Manager Fixes (#120)

--- Spaceships/app/src/main/java/com/plainsimple/spaceships/helper/GameModeManager.java
+++ Spaceships/app/src/main/java/com/plainsimple/spaceships/helper/GameModeManager.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 
 import com.plainsimple.spaceships.view.GameView;
 
+import java.util.HashMap;
+
 /**
  * Manages persistent storage of GameMode objects in SharedPreferences as well as retrieval of
  * queried GameMode objects.
@@ -18,13 +20,22 @@ public class GameModeManager {
     // available GameModes
     public static final String ENDLESS_0 = "ENDLESS_0"; // todo: efficiency
 
+    private static final HashMap<String, String> defaultStrings = getDefaultStrings();
+
+    // mapping of GameMode key constants with actual content
+    private static HashMap<String, String> getDefaultStrings() {
+        HashMap<String, String> defaults = new HashMap<>();
+        defaults.put(ENDLESS_0, ENDLESS_0_STR);
+        return defaults;
+    }
+
     // looks up the given gameModeKey in the Shared Preferences file. Parses the String and returns
     // the GameMode object resulting from it. The keys must be one of the "available GameModes"
     // defined above. Throws IllegalArgumentException if key is invalid.
     public GameMode retrieve(Context context, String gameModeKey) throws IllegalArgumentException {
         SharedPreferences data = context.getSharedPreferences(PREFERENCES_FILE_KEY, Context.MODE_PRIVATE);
-        if (data.contains(gameModeKey)) {
-            return GameMode.fromString(data.getString(gameModeKey, ""));
+        if (defaultStrings.containsKey(gameModeKey)) {
+            return GameMode.fromString(data.getString(gameModeKey, defaultStrings.get(gameModeKey)));
         } else {
             throw new IllegalArgumentException("Unrecognized key (\"" + gameModeKey + "\")");
         }
@@ -35,7 +46,7 @@ public class GameModeManager {
     public void put(Context context, String gameModeKey, GameMode gameMode) throws IllegalArgumentException {
         // get a handle to correct SharedPreferences fiel
         SharedPreferences data = context.getSharedPreferences(PREFERENCES_FILE_KEY, Context.MODE_PRIVATE);
-        if (data.contains(gameModeKey)) {
+        if (defaultStrings.containsKey(gameModeKey)) {
             data.edit().putString(gameModeKey, gameMode.toString()).commit();
         } else {
             throw new IllegalArgumentException("Unrecognized key (\"" + gameModeKey + "\")");
